3a3a547886f17aefeefbedb9398eb014e97311ad
baopingx <baopingx.zhang@intel.com>
2015-12-15 18:02:47 +0800

    commit demo android app v1.4
    fix some bug and remove load from sdcard use app intelnal
    
    Signed-off-by: baopingx <baopingx.zhang@intel.com>

--- app/src/main/java/com/example/baopingx/myapplication/getCustomeGesture.java
+++ app/src/main/java/com/example/baopingx/myapplication/getCustomeGesture.java
@@ -1,6 +1,8 @@
 package com.example.baopingx.myapplication;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Set;
 
 import android.Manifest;
 import android.app.Activity;
@@ -13,13 +15,19 @@ import android.gesture.GestureOverlayView;
 import android.gesture.GestureOverlayView.OnGestureListener;
 import android.gesture.GestureOverlayView.OnGesturePerformedListener;
 import android.gesture.Prediction;
+import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class getCustomeGesture extends AppCompatActivity {
@@ -27,26 +35,52 @@ public class getCustomeGesture extends AppCompatActivity {
     private GestureLibrary library;
     private Gesture mgesture;
     private GestureOverlayView overlayView;
+    private TelephonyManager tm;
+    private String gesPath;
+    private Button mbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.get_custome_gesture);
+/*
+        if (!Environment.MEDIA_MOUNTED.equals(Environment
+                .getExternalStorageState())) {
+            Toast.makeText(this, "SDCard不存在！", Toast.LENGTH_SHORT).show();
+            this.finish();
+        }
 
+        gesPath= new File(Environment.getExternalStorageDirectory(),
+                "gestures").getAbsolutePath();
+        File f = new File(gesPath);
+        if (!f.exists())
+        {
+            library = GestureLibraries.fromRawResource(this, R.raw.gestures);//得到手势库
+            library.load();
+            Log.e(TAG, "GestureLibraries load from app res/raw success");
+        }
+       library = GestureLibraries.fromFile(f);
+*/
         library = GestureLibraries.fromRawResource(this, R.raw.gestures);//得到手势库
-        Log.e(TAG, "GestureLibraries load success");
+        library.load();
+        final Set<String> entries = library.getGestureEntries();
+        Log.i(TAG, "GestureLibraries record = " + entries.size());
+
+        mbutton = (Button) this.findViewById(R.id.reconize);
         overlayView = (GestureOverlayView) this.findViewById(R.id.gestures);
         overlayView.addOnGestureListener(new GestureListener());//为view添加手势监听事件
     }
 
     public void find(View v) {
         recognize(mgesture);
-        overlayView.clear(true);
+        overlayView.clear(true);//clear before gesture
     }
 
     private final class GestureListener implements GestureOverlayView.OnGestureListener {
         public void onGestureStarted(GestureOverlayView overlay, MotionEvent event) {
             Log.i(TAG, "onGestureStarted()");
+            mbutton.setEnabled(true);
+
         }
 
         public void onGesture(GestureOverlayView overlay, MotionEvent event) {
@@ -74,19 +108,38 @@ public class getCustomeGesture extends AppCompatActivity {
         ArrayList<Prediction> predictions = library.recognize(gesture);
         if (!predictions.isEmpty()) {
             Prediction prediction = predictions.get(0);
-            if (prediction.score >= 6) {//匹配率大于60%
+            if (prediction.score >= 5) {//匹配率
                 if ("call".equals(prediction.name)) {
                     Log.i(TAG, "prediction.name = "+prediction.name);
-                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:1350505050"));
+                    Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:13655559737"));
                     if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
                         // TODO: Consider calling
+                        //check whether call service can use now
+                        tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+                        switch (tm.getSimState())
+                        {
+                            case TelephonyManager.SIM_STATE_ABSENT:
+                                Log.e(TAG, "no sim card");
+                                break;
+                            case TelephonyManager.SIM_STATE_UNKNOWN:
+                                Log.e(TAG, "unkonw status");
+                                break;
+                            case TelephonyManager.SIM_STATE_READY:
+                                Log.i(TAG, "sim card ready");
+                                Log.i(TAG, "now begin to start activity call");
+                                startActivity(intent);
+                        }
                         return;
                     }
-                    Log.i(TAG, "now begin to start activity call");
-                    startActivity(intent);
-                }else if("close".equals(prediction.name)){
+                }else if("exit".equals(prediction.name)){
                     Log.i(TAG, "prediction.name = "+prediction.name);
                     finish();//关闭Activity
+                }
+                else {
+                    if ("camera".equals(prediction.name)) {
+                        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                        startActivity(intent);
+                    }
                 }
             }else{
                 Toast.makeText(getApplicationContext(), R.string.low, 1).show();
